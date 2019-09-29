73e06e9731d15ef3ca56cbc29f218696d887921c
asifdev <anttor062@gmail.com>
2019-09-29 17:06:17 -0600

    Implement map

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from "./Card";
+import Cards from "./Cards";
 import "tachyons";
 import * as serviceWorker from "./serviceWorker";
 import { robots } from "./robots";
 
 ReactDOM.render(
   <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    <Cards robots={robots} />
   </div>,
   document.getElementById("root")
 );
